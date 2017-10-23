552575d16215f2fc249f9b1ada695ea90cf600df
LeereBobo <389152218@qq.com>
2017-10-23 17:56:44 +0800

    add

--- node3/readStream.js
+++ node3/readStream.js
@@ -1,8 +1,32 @@
+var http = require("http");
+
 var fs = require("fs");
-var myReadStream = fs.createReadStream(__dirname+"/readMe.txt","utf8");
+
+// 读取数据流
+// var myReadStream = fs.createReadStream(__dirname+"/readMe.txt","utf8");
 // console.log(myReadStream);
 
+// 写入数据流
+// var myWriteStream = fs.createWriteStream(__dirname+"/writeMe.txt");
+
+// 管道 将左边数据放至右边
+// myReadStream.pipe(myWriteStream);
+
+/*
 myReadStream.on("data",function(chunk){
-	console.log("================接收chunk===============")
-	console.log(chunk);
+	// console.log("================接收chunk===============")
+	// console.log(chunk);
+
+	myWriteStream.write(chunk);
 });//data不能改写
+
+*/
+
+// 创建服务器
+var server = http.createServer(function(req,res){
+	res.writeHead(200,{"Content-type":"text/plain"});
+	var myReadStream = fs.createReadStream(__dirname+"/readMe.txt","utf8");
+	myReadStream.pipe(res);
+});
+server.listen(3000,"127.0.0.1");//localhost:3000查看
+console.log("server is running...");
