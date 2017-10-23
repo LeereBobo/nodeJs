ac01d82712e9c5d7453218e7928ea9776fdf08e8
LeereBobo <32626286+LeereBobo@users.noreply.github.com>
2017-10-23 04:53:26 -0500

    Delete readStream.js

--- node3/readStream.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-var myReadStream = fs.createReadStream(__dirname+"/readMe.txt","utf8");
-// console.log(myReadStream);
-
-myReadStream.on("data",function(chunk){
-	console.log("================接收chunk===============")
-	console.log(chunk);
-});//data不能改写
