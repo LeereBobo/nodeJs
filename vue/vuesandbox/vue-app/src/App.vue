2a919d3e3db3f3599297bed15e75d1dd708e301a
LeereBobo <32626286+LeereBobo@users.noreply.github.com>
2019-08-02 21:18:04 +0800

    Update App.vue

--- vue/vuesandbox/vue-app/src/App.vue
+++ vue/vuesandbox/vue-app/src/App.vue
@@ -1,27 +1,32 @@
-<!--template:是我们需要用到的模板，模板中有且只有一个跟标签  -->
+
+<!-- 1.结构 -->
+<!-- template:是我们需要用到的模板，模板中有且只有一个根标签 -->
 <template>
   <div id="app">
-  
-   
+    
   </div>
 </template>
+
+
+<!-- 2.逻辑处理 -->
 <!-- 
-  script:标签中囊括的是所有的属性好人方法
-  export default :对外公开当前文件可被其他文件引入
-  name:给当前组件起名，此名字可以在其他组件中以标签的形式调用
-  components:承载所有当前组件需要依赖其他组件
- -->
+	(1)script:囊括所有的属性和方法
+    (2)export default:对外公开当前文件，可被其他文件引入
+    (3)name:给当前组件起名，此名可在其他组件中以标签的形式调用
+    (4)components:承载所有当前组件需要依赖其他组件 -->
 <script>
-
-
 export default {
-  name: 'app',
-  
+  name: 'App',
+  components: {
+    
+  }
 }
 </script>
-<!-- 
-     style:
- -->
+
+
+<!-- 3.样式 -->
 <style>
- 
+
 </style>
+
+
